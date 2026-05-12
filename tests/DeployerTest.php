329fbed7ecd22b09c1b18826b89acea1b3243128
Hussain <ghulamhussainbantwa@gmail.com>
2026-05-12 22:58:21 +0500

    Configure PHPUnit and add DeployerTest

---
+++ tests/DeployerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DeployerTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
